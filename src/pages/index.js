ec50dfa3e64b5de81191a78e09c2929f9f6a74a9
Ivan Limongan <ivan.limongan@gmail.com>
2019-01-17 14:03:52 -0800

    contact page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => 
     <div style={{ color: `gray` }}>
+        <Link to="/contact/">Contact</Link>
         <h2>Noice!</h2>
         <p>lol</p>
     </div>
